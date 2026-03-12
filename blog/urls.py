cc476ab91e16635212e13c10ac9257081ae2a44b
Jessye Pereira <jessyekessia16@gmail.com>
2026-03-12 18:06:30 -0300

    add mais atividades extras

--- blog/urls.py
+++ blog/urls.py
@@ -4,5 +4,10 @@ from . import views
 urlpatterns = [
   path("welcome/", views.welcome, name="welcome"),
   path("eco/<str:text>/", views.eco),
-  path("info/", views.info, name="info")
+  path("info/", views.info, name="info"),
+  path("hora/", views.hora),
+  path("login/", views.login),
+  path("lista/", views.lista),
+  path("home/", views.home, name="home"),
+  path("contato/<str:telefone>/", views.contato, name="contato")
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.http import JsonResponse
+from datetime import date
 
 # Create your views here.
 def welcome(request):
@@ -9,6 +10,23 @@ def welcome(request):
 def eco(request, text):
   return HttpResponse(f'Você digitou: {text}')
 
+# request com o meu nome
+def hora(request):
+  # envia as variáveis como contexto para ser rederizado
+  context = {
+    "nome": "Jessye",
+    "data": date.today()
+  }
+  # vai ser rederizado no home
+  return render(request, "hora.html", context)
+
+def login(request):
+  context = {
+    "is_logged_in": True,
+    "role": "admin"
+  }
+  return render(request, "login.html", context)
+
 def info(request):
   data = {
       "disciplina": "RAD",
@@ -16,3 +34,25 @@ def info(request):
       "semestre": "2026.1"
   }
   return JsonResponse(data)
+
+def lista(request):
+    produtos = [
+        {"nome": "Notebook", "preco": 3500},
+        {"nome": "Mouse", "preco": 120},
+        {"nome": "Teclado", "preco": 200},
+    ]
+
+    context = {
+        "produtos": produtos
+    }
+
+    return render(request, "lista.html", context)
+
+def home(request):
+    return render(request, "home.html")
+
+def contato(request, telefone):
+    context = {
+        "telefone": telefone
+    }
+    return render(request, "contato.html", context)
