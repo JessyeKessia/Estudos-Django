4da8c706e8b772e4b9aa00c145ba05b7183949e6
Jessye Pereira <jessyekessia16@gmail.com>
2026-04-02 14:58:08 -0300

    finalizando a atividade

--- blog/admin.py
+++ blog/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import Autor, Editora, Livro, Publica
+
+admin.site.register(Autor)
+admin.site.register(Editora)
+admin.site.register(Livro)
+admin.site.register(Publica)
